ae834b1260c826955c6ef675fe040ef680669154
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-04 17:25:58 -0300

    Prod and dev mode

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -4,9 +4,23 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const handlers = require('./lib/handlers');
 const { credentials } = require('./config');
+const morgan = require('morgan');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+  case 'production':
+    const stream = fs.createWriteStream(__dirname + '/access.log', {
+      flags: 'a',
+    });
+    app.use(morgan('combined', { stream }));
+    break;
+}
 
 // Rotas estáticas
 app.use(express.static(__dirname + '/public'));
@@ -56,10 +70,16 @@ app.use(handlers.notFound);
 app.use(handlers.serverError);
 
 if (require.main === module) {
-  app.listen(port, () =>
-    console.log(
-      `Express started on http://localhost:${port}; press Ctrl-C to terminate.`
-    )
+  app.listen(
+    port,
+    () =>
+      /* eslint-disable quotes */
+      console.log(
+        `Express started in ` +
+          `${app.get('env')} mode at http://localhost:${port}` +
+          `; press Ctrl-C to terminate.`
+      )
+    /* eslint-enable quotes */
   );
 } else {
   module.exports = app;
